04e26fd92f4b40d4394e0ce04e4d2e031d7a3090
haberger <d.haberger@homtial.com>
2024-01-18 10:19:16 +0100

    refactor, added plots

--- Environment.py
+++ Environment.py
@@ -1,12 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import math
-import random
 import heapq
 import bisect
-
+import numpy as np
+import random
+from matplotlib import pyplot as plt
+from CompanyAgent import CompanyAgent
 
 class Environment:
+
     def __init__(self, initial_market_price, agents):
         """
         Initialize the environment.
@@ -22,20 +22,11 @@ class Environment:
         self.counter = 0
         self.best_buyer_price = 0
         self.best_seller_price = np.inf
-        self.trade_history = []
+        self.buyer_hist = []
+        self.seller_hist = []
         self.sells = []
         self.buys = []
-    
-    def get_trades(self):
-        for agent in agents:
-            type, price, count = agent.post_trade()
-            for _ in range(count):
-                if type == "buy":
-                    self.buys.append((price, agent))
-                elif type == "sell":
-                    self.sells.append((price, agent))
-        self.buys = sorted(self.buys, reverse=True)
-        self.sells = sorted(self.sells)
+        self.trade_history = []
 
     def calculate_market_price(self, plot=False):
         #calc difference but only for the length of the shorter list
@@ -52,9 +43,24 @@ class Environment:
             plt.axhline(intersection_price, color="black", linestyle="--", label="Market Price")
             plt.legend()
             plt.show()
+            plt.hist(self.sells, bins=10, label="Sellers")
+            plt.hist(self.buys, bins=10, label="Buyers")
+            plt.axvline(self.market_price, color="black", linestyle="--", label="Market Price")
+            plt.legend()
+            plt.show()
     
+    def get_trades_plot(self):
+        #plot buyers and sellers
+        plt.scatter(range(len(self.buyer_hist)), [agent.expected_market_price for agent in self.buyer_hist], label="Buyers")
+        plt.scatter(range(len(self.seller_hist)), [agent.expected_market_price for agent in self.seller_hist], label="Sellers")
+        plt.axhline(self.market_price, color="black", linestyle="--", label="Market Price")
+        plt.legend()
+        plt.show()
+
     def trade(self, buyer, seller):
         self.trade_history.append(buyer.expected_market_price)
+        self.buyer_hist.append(buyer)
+        self.seller_hist.append(seller)
         buyer.allowance += 1
         buyer.count -= 1
         seller.allowance -= 1
@@ -126,6 +132,7 @@ class Environment:
             # print(f"after {self.best_seller_price=}")
 
         self.calculate_market_price(plot=True)
+        self.get_trades_plot()
         self.sellers = []
         self.buyer_heap = []
         self.sells = []
@@ -133,132 +140,3 @@ class Environment:
         self.best_buyer_price = 0
         self.best_seller_price = np.inf
         self.counter = 0
-
-class CompanyAgent:
-    def __init__(self, expected_emission, initial_allowance, sell_price, buy_price, expected_emission_noise=0.1, emission_rate_noise=0.01):
-        """
-        Initialize a Company Agent.
-
-        Parameters:
-        - expected_emission (float): Expected emission for the year.
-        - initial_allowance (float): Initial allowance for the agent.
-        - sell_price (float): Price at which the agent is willing to sell allowances.
-        - buy_price (float): Price at which the agent is willing to buy allowances.
-        """
-        self.expected_emission_noise = expected_emission_noise
-        self.emission_rate_noise = emission_rate_noise
-
-        self.initial_exepected_emission = expected_emission
-        self.initial_allowance = initial_allowance
-
-        self.expected_emission = self.initial_exepected_emission
-        self.allowance = self.initial_allowance
-
-        self.sell_price = sell_price
-        self.buy_price = buy_price  #max buying price, if higher it is not profitable to buy
-        self.emission_rate = expected_emission / 365.0 + np.random.normal(scale=self.expected_emission_noise)  # Add some noise
-
-        self.total_emission = 0
-        self.day = 1
-        self.expected_deficit = self.expected_emission - self.allowance
-
-        self.expected_market_price = (sell_price+buy_price)/2
-        self.count = 0
-        self.state = "idle"
-
-
-    def update_emission_rate(self):
-        """
-        Update the emission rate in a stochastic way.
-        """
-        # Update the emission rate with some stochastic noise
-        self.emission_rate = max(0, self.emission_rate + np.random.normal(scale=self.emission_rate_noise))
-
-    def track_emission(self):
-        """
-        Track the emissions produced by the company.
-        """
-        self.total_emission += self.emission_rate
-
-    def update_expected_emission(self):
-        """
-        Update the expected emission for the year.
-        """
-        self.expected_emission = self.total_emission/self.day * 365
-        self.day += 1
-        self.expected_deficit = self.expected_emission - self.allowance
-
-    def update_market_position(self):
-        """
-        post trade is necessary
-        """
-        if self.expected_deficit > 0:
-            #buy
-            self.count = math.ceil(self.expected_deficit)
-            self.state = "buy"
-        elif self.expected_deficit < -1:
-            #sell
-            self.count = (-1)*math.ceil(self.expected_deficit)
-            self.state = "sell"
-        else:
-            self.state = "idle"
-            self.count = 0
-
-
-    def update_agent(self):
-        """
-        Update the agent.
-        """
-
-        self.update_emission_rate()
-        self.track_emission()
-        self.update_expected_emission()
-        self.update_market_position()
-
-    def __lt__(self, other):
-        """
-        Compare two Company Agents.
-        """
-        # return randomly true or false
-        return np.random.choice([True, False])
-
-    def __str__(self):
-        """
-        String representation of the Company Agent.
-        """
-        return f"Company Agent - Initial Expected Emission: {self.initial_exepected_emission}, Initial Allowance: {self.initial_allowance}, " \
-               f"Sell Price: {self.sell_price}, Buy Price: {self.buy_price}, Emission Rate: {self.emission_rate} " \
-               f"Total Emission: {self.total_emission}, Expected Emission: {self.expected_emission}, Allowance: {self.allowance}"
-    
-    def get_sell_price(self):
-        return self.sell_price
-    
-    def get_buy_price(self):
-        return self.buy_price
-
-
-
-if __name__ == "__main__":
-    random.seed(42)
-    # create two agents
-    # define 100 agents with random expected emission, initial allowance, sell price and buy price
-    agents = []
-    for i in range(10000):
-        expected_emission = np.random.uniform(0, 100)
-        initial_allowance = np.random.uniform(0, 100)
-        sell_price = np.random.uniform(0, 100)
-        buy_price = np.random.uniform(0, 100)
-        agents.append(CompanyAgent(expected_emission, initial_allowance, sell_price, buy_price))
-
-    
-    env = Environment(5, agents)
-    env.update()
-    env = Environment(5, agents)
-    env.update()
-    env = Environment(5, agents)
-    env.update()
-    #plot hist of trades
-    plt.hist(env.trade_history, bins=10)
-    plt.show()
-    print("good_bye")
-
